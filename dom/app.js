eb87fc47a5e269f09a61d016b4d831bc9d48b853
sebine2704 <161808827+sebine2704@users.noreply.github.com>
2024-04-15 13:44:17 +0400

    Add files via upload

---
+++ dom/app.js
@@ -0,0 +1,28 @@
+// const inp = document.querySelector(".inp");
+// // console.log(inp);
+// const btn = document.querySelector(".btn");
+// const text = document.querySelector(".text");
+// // console.log(p1);
+// // console.log(btn);
+// // btn.addEventListener("click", () => {
+// //   console.log(inp.value);
+// // });
+
+// //task2
+
+// btn.addEventListener("click", () => {
+//   if (inp.value.length > 8) {
+//     console.log("sifre uzundur");
+//     text.innerText = "sifre uzundur";
+//     text.style.color = "red";
+//   } else {
+//     console.log("sifre ugurludur");
+//     text.innerText = "sifre ugurludur";
+//     text.style.color = "green";
+//   }
+// });
+
+const acc1 = document.querySelector(".acc1");
+acc1.addEventListener("click" () => {
+    console.log("fidan")
+})
