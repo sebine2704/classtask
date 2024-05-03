5e4fa1c89e9467c9638b84981725e02028f45ae6
sebine2704 <161808827+sebine2704@users.noreply.github.com>
2024-05-03 13:39:10 +0400

    Add files via upload

---
+++ login/src/data/login.jsx
@@ -0,0 +1,7 @@
+// import React from "react";
+
+const login = () => {
+  return <>login</>;
+};
+
+export default login;
---
+++ login/src/data/register.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import 
+
+const register = () => {
+  return <>register</>;
+};
+
+export default register;
